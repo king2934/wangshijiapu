c3855f5a0ea47e0cbfc9c9804a07189ccfe3fb15
king2934 <king2934@gmail.com>
2019-04-11 17:40:47 +0800

    添加了 activity背景图片

--- app/src/main/java/com/wangshijiapu/wsjp/MainActivity.java
+++ app/src/main/java/com/wangshijiapu/wsjp/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static java.lang.Thread.sleep;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.activity_main);
     }
 }
