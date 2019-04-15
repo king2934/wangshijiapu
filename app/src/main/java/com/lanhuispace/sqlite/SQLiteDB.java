fcc20393ce868ea764866d59d0fe9f0d8ff04f86
king2934 <king2934@gmail.com>
2019-04-15 14:43:51 +0800

    更新一些bug

--- app/src/main/java/com/lanhuispace/sqlite/SQLiteDB.java
+++ app/src/main/java/com/lanhuispace/sqlite/SQLiteDB.java
@@ -3,6 +3,7 @@ package com.lanhuispace.sqlite;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -14,10 +15,12 @@ public class SQLiteDB extends SQLiteOpenHelper {
 	// 传递数据库名与版本号给父类
     public SQLiteDB(@Nullable Context context, @Nullable SQLiteDatabase.CursorFactory factory) {
         super(context, DB_NAME, factory, DB_VERSION);
+        Log.d("getdata","new SQLiteDB");
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.d("getdata","init 初始化数据库");
 		this.init_db(db);//初始化数据库
     }
 
@@ -33,6 +36,7 @@ public class SQLiteDB extends SQLiteOpenHelper {
 	
 	//初始化表 - 字辈
 	public void init_table_zibei(SQLiteDatabase db) {
+        Log.d("getdata","start create数据库表");
 		String tb_sql = "create table if not exists " + TABLE_ZIBEI + " ( "
 		+"id integer primary key autoincrement,"
 		+"sort integer not null ,"
--- app/src/main/java/com/wangshijiapu/wsjp/MainActivity.java
+++ app/src/main/java/com/wangshijiapu/wsjp/MainActivity.java
@@ -1,20 +1,31 @@
 package com.wangshijiapu.wsjp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lanhuispace.http.GetJson;
+import com.lanhuispace.sqlite.SQLiteDB;
+import com.wangshijiapu.wsjp.services.InitService;
 
 import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startService(new Intent(this,InitService.class));
         try {
             sleep(3000);
         } catch (InterruptedException e) {
@@ -22,9 +33,20 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.id_main_TextView);
-
+        final TextView tv = findViewById(R.id.id_main_TextView);
         tv.setText("wangshijiapu");
+
+        Button btn = findViewById(R.id.id_main_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("开始改变");
+                SQLiteDB sdb = new SQLiteDB(null,null);
+                SQLiteDatabase db = sdb.getReadableDatabase();
+                Cursor cursor = db.query("zibei",new String[]{"id","name"},"id=?",new String[]{"1"},null, null, null, null);
+                Toast.makeText(getApplicationContext(),"Button点击事件1",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
