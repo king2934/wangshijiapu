5c2896d6467bdc84b47d7eefee9ac0d85e39448f
king2934 <king2934@gmail.com>
2019-04-18 17:14:41 +0800

    增加底部导航功能

--- app/src/main/java/com/wangshijiapu/wsjp/MainActivity.java
+++ app/src/main/java/com/wangshijiapu/wsjp/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.wangshijiapu.wsjp.db.SQLiteDB;
 import com.wangshijiapu.wsjp.services.InitService;
 
@@ -14,6 +16,7 @@ import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected BottomNavigationBar mBottomNavigationBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +31,29 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_main);
         //todo
+        this.initBottomNavigationBar();
+
     }
 
+    private void initBottomNavigationBar(){
+        mBottomNavigationBar = findViewById(R.id.bottom_navigation_bar);
+
+        mBottomNavigationBar
+			.setMode(BottomNavigationBar.MODE_FIXED)
+            .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
+
+        mBottomNavigationBar
+			.setActiveColor(R.color.colorAccent)//选中颜色 图标和文字
+            .setInActiveColor(R.color.DarkGray)//默认未选择颜色
+            .setBarBackgroundColor(R.color.White);//默认背景色
+		
+        mBottomNavigationBar
+			.addItem(new BottomNavigationItem(R.drawable.home_light,"首页"))
+			.addItem(new BottomNavigationItem(R.drawable.circle,"谱系"))
+			.addItem(new BottomNavigationItem(R.drawable.my_light,"个人"))
+			.setFirstSelectedPosition(0)
+			.initialise();
+    }
 
     public void btnGetUpdatedon(View view) {
         SQLiteDB sdb = new SQLiteDB(getBaseContext(),null);
